8c0f67fa89319d81e3b3fab45dc63e75643b5804
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-12 06:25:51 +0700

    team seeder

---
+++ database/seeders/TeamSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Team;
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class TeamSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Team::create([
+            'name' => 'Administrator',
+            'user_id' => User::first()->id,
+            'personal_team' => true,
+        ]);
+    }
+}
