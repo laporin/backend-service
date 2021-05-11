1883f8ec9329438a0ed318d3ca1bef5bb7ed72ee
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-11 09:25:18 +0700

    add voyager

--- routes/web.php
+++ routes/web.php
@@ -20,3 +20,8 @@ Route::get('/', function () {
 Route::middleware(['auth:sanctum', 'verified'])->get('/dashboard', function () {
     return view('dashboard');
 })->name('dashboard');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
