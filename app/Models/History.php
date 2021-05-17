48c258a46aefe18dfda7c345cac608a1c1330f41
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-18 01:03:08 +0700

    history model

---
+++ app/Models/History.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class History extends Model
+{
+    use HasFactory;
+
+    /**
+     * Get all of the report's images.
+     */
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
+}
