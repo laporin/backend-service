9bba95388256f171a5a0f56823122e86e9d3d6bc
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-18 01:03:18 +0700

    report model

--- app/Models/Report.php
+++ app/Models/Report.php
@@ -8,4 +8,12 @@ use Illuminate\Database\Eloquent\Model;
 class Report extends Model
 {
     use HasFactory;
+
+    /**
+     * Get all of the report's images.
+     */
+    public function images()
+    {
+        return $this->morphMany(Image::class, 'imageable');
+    }
 }
