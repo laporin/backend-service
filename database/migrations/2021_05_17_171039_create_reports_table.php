40acf47d9001e6706dfe8e23294c9a8b174cc21b
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-18 00:23:57 +0700

    create reports

---
+++ database/migrations/2021_05_17_171039_create_reports_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateReportsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reports', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id');
+            $table->foreignId('category_id');
+            $table->text('detail');
+            $table->enum('status', ['waiting', 'process', 'disposition', 'coordination', 'done']);
+            $table->text('address');
+            $table->string('city');
+            $table->string('subdistrict');
+            $table->double('latitude');
+            $table->double('longitude');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reports');
+    }
+}
