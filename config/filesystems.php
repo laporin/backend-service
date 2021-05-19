cc80dd5aebe2d94d63b9e3f1cee684f577d029a2
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-19 21:02:45 +0700

    media path

--- config/filesystems.php
+++ config/filesystems.php
@@ -30,6 +30,11 @@ return [
 
     'disks' => [
 
+        'media' => [
+            'driver' => 'local',
+            'root'   => storage_path('media'),
+        ],
+
         'local' => [
             'driver' => 'local',
             'root' => storage_path('app'),
@@ -38,7 +43,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/public'),
-            'url' => env('APP_URL').'/storage',
+            'url' => env('APP_URL') . '/storage',
             'visibility' => 'public',
         ],
 
