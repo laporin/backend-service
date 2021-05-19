52109500f9b24d62cfa57ef12289e0e86eb5c9c0
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-19 20:58:15 +0700

    add report seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,6 +20,7 @@ class DatabaseSeeder extends Seeder
             UserSeeder::class,
             AgentSeeder::class,
             CategorySeeder::class,
+            ReportSeeder::class,
         ]);
     }
 }
