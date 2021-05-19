48a152317fa470285929e63606e35ca90fe18f9d
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-19 18:16:54 +0700

    all instead of paginate

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -16,7 +16,7 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return new CategoryCollection(Category::paginate());
+        return new CategoryCollection(Category::all());
     }
 
     /**
