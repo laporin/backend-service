133285db1d2ebd23190fb41f6cfada28ae465c89
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-19 20:57:30 +0700

    change to name

--- database/migrations/2021_05_17_175645_create_images_table.php
+++ database/migrations/2021_05_17_175645_create_images_table.php
@@ -15,7 +15,7 @@ class CreateImagesTable extends Migration
     {
         Schema::create('images', function (Blueprint $table) {
             $table->id();
-            $table->string('url');
+            $table->string('name');
             $table->foreignId('imageable_id');
             $table->string('imageable_type');
             $table->timestamps();
