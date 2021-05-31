139936004bdcbcfa29fab2960fae761fc3d89f3c
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-05-31 20:10:26 +0700

    rename to service account

--- config/filesystems.php
+++ config/filesystems.php
@@ -60,7 +60,7 @@ return [
         'gcs' => [
             'driver' => 'gcs',
             'project_id' => env('GOOGLE_CLOUD_PROJECT_ID', 'aplikasi-laporin'),
-            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', base_path('aplikasi-laporin-account.json')), // optional: /path/to/service-account.json
+            'key_file' => env('GOOGLE_CLOUD_KEY_FILE', base_path('aplikasi-laporin-service-account.json')), // optional: /path/to/service-account.json
             'bucket' => env('GOOGLE_CLOUD_STORAGE_BUCKET', 'backend-service-bucket'),
             'path_prefix' => env('GOOGLE_CLOUD_STORAGE_PATH_PREFIX', null), // optional: /default/path/to/apply/in/bucket
             'storage_api_uri' => env('GOOGLE_CLOUD_STORAGE_API_URI', null), // see: Public URLs below
