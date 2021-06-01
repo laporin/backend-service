78694344ec1e60167943398eeda7409f93d0ad38
Arif Ikhsanudin <arif.ikhsanudin.id@gmail.com>
2021-06-02 06:54:28 +0700

    add user to auth response

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -32,6 +32,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
+            'user' => $user,
         ]);
     }
 
@@ -50,6 +51,7 @@ class AuthController extends Controller
         return response()->json([
             'access_token' => $token,
             'token_type' => 'Bearer',
+            'user' => $user,
         ]);
     }
 
